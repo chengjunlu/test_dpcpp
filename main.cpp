e37cc8af22b9a37cf10ff41626f7aef65d4bf105
johnlu <chengjun.lu@intel.com>
2021-11-23 22:43:12 +0800

    Update the unit test on IGC codegen

--- main.cpp
+++ main.cpp
@@ -294,30 +294,49 @@ static inline void launch_unrolled_kernel(
       int i = 0;
       int linear_idx = thread_idx * vec_size + i;
 #ifdef CALL_AS_MEMBER_FUNC
-      auto offset = ic.get(linear_idx);
-#else
-//      auto offset = get<arity>(linear_idx, ic.dims, ic.sizes_, ic.strides_);
-        using offset_type = Array<int32_t, arity>;
-        offset_type offset;
+      using offset_type = Array<int32_t, arity>;
+      offset_type offset;
+#pragma unroll
+      for (int arg = 0; arg < arity; arg++) {
+        offset[arg] = 0;
+      }
+
+#pragma unroll
+      for (int dim = 0; dim < 12; ++dim) {
+        if (dim == ic.dims) {
+          break;
+        }
+        auto divmod = ic.sizes_[dim].divmod(linear_idx);
+        linear_idx = divmod.div;
+
 #pragma unroll
         for (int arg = 0; arg < arity; arg++) {
-          offset[arg] = 0;
+          offset[arg] += divmod.mod * ic.strides_[arity * dim + arg];
         }
+      }
+#else
+      using offset_type = Array<int32_t, arity>;
+      offset_type offset;
+#pragma unroll
+      for (int arg = 0; arg < arity; arg++) {
+        offset[arg] = 0;
+      }
 
 #pragma unroll
-        for (int dim = 0; dim < 12; ++dim) {
-          if (dim == ic.dims) {
-            break;
-          }
-          auto divmod = ic.sizes_[dim].divmod(linear_idx);
-          linear_idx = divmod.div;
+      for (int dim = 0; dim < 12; ++dim) {
+//        if (dim == ic.dims) {
+//          break;
+//        }
+        auto divmod = ic.sizes_[dim].divmod(linear_idx);
+        linear_idx = divmod.div;
 
 #pragma unroll
-          for (int arg = 0; arg < arity; arg++) {
-            offset[arg] += divmod.mod * ic.strides_[arity * dim + arg];
-          }
+        for (int arg = 0; arg < arity; arg++) {
+          offset[arg] += divmod.mod * ic.strides_[arity * dim + arg];
         }
+      }
 #endif
+
       static_unroll<unroll_load_test, arity>::with_args(data_ptr, args, offset, l, i, 1);
 
       // unroll the compute multiple times
