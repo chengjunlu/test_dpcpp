1f8dd26e1da4fc98253ab0799bed1e2566e945c0
Lu, Chengjun <chengjun.lu@intel.com>
2024-07-11 10:41:42 +0000

    Enhance the test results print.

--- test_threads_spawning_overhead/ThreadsSpawnTest.cpp
+++ test_threads_spawning_overhead/ThreadsSpawnTest.cpp
@@ -13,6 +13,7 @@
 #ifdef __SYCL_DEVICE_ONLY__
 SYCL_EXTERNAL extern "C" ulong __builtin_spirv_OpReadClockKHR_i64_i32(uint scope);
 SYCL_EXTERNAL extern "C" OVERLOADABLE ulong intel_get_cycle_counter( void );
+SYCL_EXTERNAL uint OVERLOADABLE intel_get_slice_id( void );
 SYCL_EXTERNAL uint OVERLOADABLE intel_get_subslice_id( void );
 SYCL_EXTERNAL uint OVERLOADABLE intel_get_eu_id( void );
 #define __SYCL_CONSTANT_AS __attribute__((opencl_constant))
@@ -21,6 +22,9 @@ ulong intel_get_cycle_counter( void ) {
   return 0;
 }
 ulong __builtin_spirv_OpReadClockKHR_i64_i32(uint scope) {
+  return 0;
+}
+uint intel_get_slice_id( void ) {
   return 0;
 }
 uint intel_get_subslice_id( void ) {
@@ -52,6 +56,7 @@ void test_subgroup_ballot(sycl::queue& queue){
 
   ulong* time_entry = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* time_exit = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
+  ulong* slice_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* sub_slice_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* eu_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
 
@@ -65,10 +70,12 @@ void test_subgroup_ballot(sycl::queue& queue){
                   auto start_time = __builtin_spirv_OpReadClockKHR_i64_i32(0);
                   time_entry[work_group_linear_id] = start_time;
 
+                  slice_id[work_group_linear_id] = intel_get_slice_id();
                   sub_slice_id[work_group_linear_id] = intel_get_subslice_id();
+                  eu_id[work_group_linear_id] = intel_get_eu_id();
+                  // touch the SLM
                   auto a = local_buffer[work_group_linear_id];
                   local_buffer[work_group_linear_id] = a + 1;
-                  eu_id[work_group_linear_id] = intel_get_eu_id();
 
                   auto end_time = __builtin_spirv_OpReadClockKHR_i64_i32(0);
                   time_exit[work_group_linear_id] = end_time;
@@ -77,12 +84,14 @@ void test_subgroup_ballot(sycl::queue& queue){
 
   queue.submit(cgf);
   queue.wait();
-  print_time("time_entry", queue, time_entry, NUM_WORK_GROUP);
-  print_time("time_exit", queue, time_exit, NUM_WORK_GROUP);
-  print_time("sub_slice_id", queue, sub_slice_id, NUM_WORK_GROUP);
-  print_time("eu_id", queue, eu_id, NUM_WORK_GROUP);
+//  print_time("time_entry", queue, time_entry, NUM_WORK_GROUP);
+//  print_time("time_exit", queue, time_exit, NUM_WORK_GROUP);
+//  print_time("slice_id", queue, slice_id, NUM_WORK_GROUP);
+//  print_time("sub_slice_id", queue, sub_slice_id, NUM_WORK_GROUP);
+//  print_time("eu_id", queue, eu_id, NUM_WORK_GROUP);
   ulong* time_entry_host = to_host(queue, time_entry, NUM_WORK_GROUP);
   ulong* time_exit_host = to_host(queue, time_exit, NUM_WORK_GROUP);
+  ulong* slice_id_host = to_host(queue, slice_id, NUM_WORK_GROUP);
   ulong* sub_slice_id_host = to_host(queue, sub_slice_id, NUM_WORK_GROUP);
   ulong* eu_id_host = to_host(queue, eu_id, NUM_WORK_GROUP);
 
@@ -90,20 +99,22 @@ void test_subgroup_ballot(sycl::queue& queue){
     ulong work_group_id;
     ulong entry;
     ulong exit;
+    ulong slice_id;
     ulong sub_slice_id;
     ulong eu_id;
   };
 
-  std::map<unsigned, std::vector<struct time_stamp>> time_map;
+  std::map<std::tuple<unsigned, unsigned>, std::vector<struct time_stamp>> time_map;
 
   for (unsigned i = 0; i < NUM_WORK_GROUP; ++i) {
     struct time_stamp ts;
     ts.work_group_id = i;
     ts.entry = time_entry_host[i];
     ts.exit = time_exit_host[i];
+    ts.slice_id = slice_id_host[i];
     ts.sub_slice_id = sub_slice_id_host[i];
     ts.eu_id = eu_id_host[i];
-    time_map[ts.sub_slice_id].push_back(ts);
+    time_map[{ts.slice_id, ts.sub_slice_id}].push_back(ts);
   }
 
   std::vector<struct time_stamp> time_stamp;
@@ -118,17 +129,17 @@ void test_subgroup_ballot(sycl::queue& queue){
   for (unsigned i = 0; i < time_stamp.size(); i++) {
     auto& ts = time_stamp[i];
     if (i == 0) {
-      printf("john lu work_group_id: %ld, sub_slice_id: %ld, entry: %ld, exit: %ld\n", ts.work_group_id, ts.sub_slice_id, ts.entry,
+      printf("john lu work_group_id: %ld, slice_id:%ld, sub_slice_id: %ld, eu_id: %ld, entry: %ld, exit: %ld\n", ts.work_group_id, ts.slice_id, ts.sub_slice_id, ts.eu_id, ts.entry,
              ts.exit);
       continue;
     }
     auto& prev_ts = time_stamp[i-1];
-    printf("john lu work_group_id: %ld, sub_slice_id: %ld, entry: %ld, exit: %ld, diff start: %ld\n", ts.work_group_id, ts.sub_slice_id, ts.entry,
+    printf("john lu work_group_id: %ld, slice_id:%ld, sub_slice_id: %ld, eu_id: %ld, entry: %ld, exit: %ld, diff start: %ld\n", ts.work_group_id, ts.slice_id, ts.sub_slice_id, ts.eu_id, ts.entry,
            ts.exit, ts.entry - prev_ts.entry);
   }
 
   for (auto& kv : time_map) {
-    printf("sub_slice_id: %d, total work group num: %zu\n", kv.first, kv.second.size());
+    printf("slice_id: %d, sub_slice_id: %d, total work group num: %zu\n", std::get<0>(kv.first), std::get<1>(kv.first), kv.second.size());
     std::vector<long> diff;
     auto time_stamp = kv.second;
     std::sort(time_stamp.begin(), time_stamp.end(), [](const struct time_stamp& a, const struct time_stamp& b) {
--- test_threads_spawning_overhead/main.cpp
+++ test_threads_spawning_overhead/main.cpp
@@ -24,6 +24,11 @@ int main() {
 
   std::cout << "root device count" << root_devices.size() << std::endl;
   std::cout << "run test on device:" << root_devices[0].get_info<sycl::info::device::name>() << std::endl;
+  std::cout << "      slice number:" << root_devices[0].get_info<sycl::ext::intel::info::device::gpu_slices>() << std::endl;
+  std::cout << "   subslice number:" << root_devices[0].get_info<sycl::ext::intel::info::device::gpu_subslices_per_slice>() << std::endl;
+  std::cout << "         eu number:" << root_devices[0].get_info<sycl::ext::intel::info::device::gpu_eu_count_per_subslice>() << std::endl;
+  std::cout << "physthreads number:" << root_devices[0].get_info<sycl::ext::intel::info::device::gpu_hw_threads_per_eu>() << std::endl;
+  std::cout << " simd width number:" << root_devices[0].get_info<sycl::ext::intel::info::device::gpu_eu_simd_width>() << std::endl;
   sycl::queue queue = sycl::queue(root_devices[0], {property::queue::in_order(),
            property::queue::enable_profiling()});
 
