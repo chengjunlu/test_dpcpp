4fffec56b6d5e608d88aa1165bc54f14ccd246b6
Lu, Chengjun <chengjun.lu@intel.com>
2024-07-11 11:50:24 +0000

    Enhance the test results print.

--- test_threads_spawning_overhead/ThreadsSpawnTest.cpp
+++ test_threads_spawning_overhead/ThreadsSpawnTest.cpp
@@ -5,7 +5,7 @@
 #include <map>
 #include <vector>
 
-#define NUM_WORK_GROUP 64
+#define NUM_WORK_GROUP 256
 #define SLM_SIZE 64 * 1024
 #define OVERLOADABLE __attribute__((overloadable))
 #define FREQUENCY 1.6e9
@@ -13,6 +13,7 @@
 #ifdef __SYCL_DEVICE_ONLY__
 SYCL_EXTERNAL extern "C" ulong __builtin_spirv_OpReadClockKHR_i64_i32(uint scope);
 SYCL_EXTERNAL extern "C" OVERLOADABLE ulong intel_get_cycle_counter( void );
+SYCL_EXTERNAL uint OVERLOADABLE intel_get_dual_subslice_id( void );
 SYCL_EXTERNAL uint OVERLOADABLE intel_get_slice_id( void );
 SYCL_EXTERNAL uint OVERLOADABLE intel_get_subslice_id( void );
 SYCL_EXTERNAL uint OVERLOADABLE intel_get_eu_id( void );
@@ -22,6 +23,9 @@ ulong intel_get_cycle_counter( void ) {
   return 0;
 }
 ulong __builtin_spirv_OpReadClockKHR_i64_i32(uint scope) {
+  return 0;
+}
+uint intel_get_dual_subslice_id( void ) {
   return 0;
 }
 uint intel_get_slice_id( void ) {
@@ -56,6 +60,7 @@ void test_subgroup_ballot(sycl::queue& queue){
 
   ulong* time_entry = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* time_exit = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
+  ulong* dual_subslice_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* slice_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* sub_slice_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
   ulong* eu_id = (ulong*)malloc_device(sizeof(ulong)* NUM_WORK_GROUP, queue);
@@ -70,6 +75,7 @@ void test_subgroup_ballot(sycl::queue& queue){
                   auto start_time = __builtin_spirv_OpReadClockKHR_i64_i32(0);
                   time_entry[work_group_linear_id] = start_time;
 
+                  dual_subslice_id[work_group_linear_id] = intel_get_dual_subslice_id();
                   slice_id[work_group_linear_id] = intel_get_slice_id();
                   sub_slice_id[work_group_linear_id] = intel_get_subslice_id();
                   eu_id[work_group_linear_id] = intel_get_eu_id();
@@ -92,6 +98,7 @@ void test_subgroup_ballot(sycl::queue& queue){
   ulong* time_entry_host = to_host(queue, time_entry, NUM_WORK_GROUP);
   ulong* time_exit_host = to_host(queue, time_exit, NUM_WORK_GROUP);
   ulong* slice_id_host = to_host(queue, slice_id, NUM_WORK_GROUP);
+  ulong* dual_subslice_id_host = to_host(queue, dual_subslice_id, NUM_WORK_GROUP);
   ulong* sub_slice_id_host = to_host(queue, sub_slice_id, NUM_WORK_GROUP);
   ulong* eu_id_host = to_host(queue, eu_id, NUM_WORK_GROUP);
 
@@ -99,12 +106,13 @@ void test_subgroup_ballot(sycl::queue& queue){
     ulong work_group_id;
     ulong entry;
     ulong exit;
+    ulong dual_subslice_id;
     ulong slice_id;
     ulong sub_slice_id;
     ulong eu_id;
   };
 
-  std::map<std::tuple<unsigned, unsigned>, std::vector<struct time_stamp>> time_map;
+  std::map<std::tuple<unsigned, unsigned, unsigned>, std::vector<struct time_stamp>> time_map;
 
   for (unsigned i = 0; i < NUM_WORK_GROUP; ++i) {
     struct time_stamp ts;
@@ -112,9 +120,10 @@ void test_subgroup_ballot(sycl::queue& queue){
     ts.entry = time_entry_host[i];
     ts.exit = time_exit_host[i];
     ts.slice_id = slice_id_host[i];
+    ts.dual_subslice_id = dual_subslice_id_host[i];
     ts.sub_slice_id = sub_slice_id_host[i];
     ts.eu_id = eu_id_host[i];
-    time_map[{ts.slice_id, ts.sub_slice_id}].push_back(ts);
+    time_map[{ts.slice_id, ts.dual_subslice_id, ts.sub_slice_id}].push_back(ts);
   }
 
   std::vector<struct time_stamp> time_stamp;
@@ -126,20 +135,22 @@ void test_subgroup_ballot(sycl::queue& queue){
     return a.entry < b.entry;
   });
 
+#if 0
   for (unsigned i = 0; i < time_stamp.size(); i++) {
     auto& ts = time_stamp[i];
     if (i == 0) {
-      printf("john lu work_group_id: %ld, slice_id:%ld, sub_slice_id: %ld, eu_id: %ld, entry: %ld, exit: %ld\n", ts.work_group_id, ts.slice_id, ts.sub_slice_id, ts.eu_id, ts.entry,
+      printf("john lu work_group_id: %ld, slice_id:%ld, dual_subslice_id: %ld, sub_slice_id: %ld, eu_id: %ld, entry: %ld, exit: %ld\n", ts.work_group_id, ts.slice_id, ts.dual_subslice_id, ts.sub_slice_id, ts.eu_id, ts.entry,
              ts.exit);
       continue;
     }
     auto& prev_ts = time_stamp[i-1];
-    printf("john lu work_group_id: %ld, slice_id:%ld, sub_slice_id: %ld, eu_id: %ld, entry: %ld, exit: %ld, diff start: %ld\n", ts.work_group_id, ts.slice_id, ts.sub_slice_id, ts.eu_id, ts.entry,
+    printf("john lu work_group_id: %ld, slice_id:%ld, dual_subslice_id: %ld, sub_slice_id: %ld, eu_id: %ld, entry: %ld, exit: %ld, diff start: %ld\n", ts.work_group_id, ts.slice_id, ts.dual_subslice_id, ts.sub_slice_id, ts.eu_id, ts.entry,
            ts.exit, ts.entry - prev_ts.entry);
   }
+#endif
 
   for (auto& kv : time_map) {
-    printf("slice_id: %d, sub_slice_id: %d, total work group num: %zu\n", std::get<0>(kv.first), std::get<1>(kv.first), kv.second.size());
+    printf("slice_id: %d, dual_subslice_id: %ld, sub_slice_id: %d, total work group num: %zu\n", std::get<0>(kv.first), std::get<1>(kv.first), std::get<2>(kv.first), kv.second.size());
     std::vector<long> diff;
     auto time_stamp = kv.second;
     std::sort(time_stamp.begin(), time_stamp.end(), [](const struct time_stamp& a, const struct time_stamp& b) {
